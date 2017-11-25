72b589909a682a5d0c7642886e38b93f4bbb7189
chiu <tim556612378@gmail.com>
2017-11-25 19:04:02 +0800

    Enable the program marking function in 振宗's project to be called by any other webpage using ajax. (Please refer to testajax.aspx for  the ajax usage  )

---
+++ Exercise/MarkProgramQuestion.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using gradeprogram.Service;
+using gradeprogram.Service.Interface;
+using gradeprogram.Models;
+
+public partial class Exercise_MarkProgramQuestion : System.Web.UI.Page
+{
+    private ITaskCorrection TaskCorrectionService;
+    protected void Page_Load(object sender, EventArgs e)
+    { 
+        string ProgramPath=Request.Form["ProgramPath"];
+        string cQID = Request.Form["cQID"];
+        string StuCouHWDe_ID = Request.Form["StuCouHWDe_ID"];
+        string questionNum = Request.Form["questionNum"];
+        string result= MarkProgram(ProgramPath,cQID,StuCouHWDe_ID,questionNum);
+        Response.Write(result);
+        Response.End();
+    }
+
+
+    //if StuCouHWDe_ID is null, just mark all the programs in the folder.
+    [WebMethod]
+    public string MarkProgram(string ProgramPath,string cQID,string StuCouHWDe_ID,string questionNum)
+    {
+        //call program marker
+        this.TaskCorrectionService = new TaskCorrection();
+        return TaskCorrectionService.CorrectTask( ProgramPath,cQID, StuCouHWDe_ID, questionNum);
+        /*
+        string strUpdateSQL = "UPDATE RecordMultiFormItem SET Required='true' WHERE MFID='" + FormID + "'";
+        CsDBConnection.InsertDataTable(strUpdateSQL);
+        return FormID + DateTime.Now.ToString();
+        */
+    }
+}
---
+++ Exercise/testajax.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="testajax.aspx.cs" Inherits="Exercise_testajax" %>
+
+<!DOCTYPE html>
+<script src="../Scripts/jquery-1.11.2.min.js"></script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+<script>
+    $.ajax(
+                             {
+                                 url: "MarkProgramQuestion.aspx",
+                                 data: "{'ProgramPath':'dasd','cQID':'sdad','StuCouHWDe_ID':'all','questionNum':'Q4'}",
+                                 type: "POST",
+                                 cache: false,
+                                 async: false,
+                                 dataType:"text",
+                                 success: function (data) {
+                                     alert(data);
+                                 },
+                                 error:function(){alert('ajax failed')}
+                             });
+
+</script>
+</html>
